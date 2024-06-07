b120ae2e15fc50fc4670a2d12c3bc06bb305bf9d
apprajapati <apprajapati@hotmail.com>
2024-06-07 08:41:12 -0400

    added listColumn to display all tasks from database

--- app/src/main/java/com/apprajapati/compose_todo/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/apprajapati/compose_todo/ui/screens/list/ListContent.kt
@@ -1,13 +1,12 @@
 package com.apprajapati.compose_todo.ui.screens.list
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,8 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,8 +24,20 @@ import com.apprajapati.compose_todo.ui.components.PriorityItem
 import com.apprajapati.compose_todo.ui.theme.LARGE_PADDING
 
 @Composable
-fun ListContent(task: Task, navigateToTaskScreen: (taskId: Int) -> Unit) {
-    TaskItem(todoTask = task, navigateToTaskScreen)
+fun ListContent(tasks: List<Task>, navigateToTaskScreen: (taskId: Int) -> Unit) {
+
+    LazyColumn {
+
+        item(contentType = tasks) {
+            for (task in tasks) {
+                TaskItem(todoTask = task) {
+                    navigateToTaskScreen(it)
+                }
+            }
+        }
+
+
+    }
 }
 
 @Composable
@@ -36,9 +45,11 @@ fun TaskItem(todoTask: Task, navigateToTaskScreen: (taskId: Int) -> Unit) {
 
     Surface(
         modifier = Modifier
-            .fillMaxWidth(),
-        shape = RectangleShape,
-        shadowElevation = 2.dp,
+            .fillMaxWidth()
+            .padding(5.dp),
+        shape = RoundedCornerShape(10.dp),
+        shadowElevation = 1.dp,
+        tonalElevation = 1.dp,
         onClick = {
             navigateToTaskScreen(todoTask.id)
         }) {
--- app/src/main/java/com/apprajapati/compose_todo/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/apprajapati/compose_todo/ui/screens/list/ListScreen.kt
@@ -8,14 +8,14 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.apprajapati.compose_todo.R
-import com.apprajapati.compose_todo.data.models.Priority
-import com.apprajapati.compose_todo.data.models.Task
 import com.apprajapati.compose_todo.ui.screens.appbar.ListAppbar
 import com.apprajapati.compose_todo.ui.viewmodels.SharedViewModel
 import com.apprajapati.compose_todo.util.SearchAppbarState
@@ -26,17 +26,19 @@ fun ListScreen(viewModel: SharedViewModel, navigateToTaskScreen: (taskId: Int) -
     val searchAppbarState: SearchAppbarState by viewModel.searchAppbarState
     val searchStringState: String by viewModel.searchTextState
 
+    LaunchedEffect(key1 = true) {
+        viewModel.getAllTasks()
+    }
+
+    val allTasks by viewModel.allTasks.collectAsState() //instead of passing allTasks.value from StateFlow, you can use "by" keyword in allTasks declaration
+
     Scaffold(topBar = {
         ListAppbar(viewModel, searchAppbarState, searchStringState)
     },
         content = { padding ->
             Column(modifier = Modifier.padding(padding)) {
                 ListContent(
-                    Task(
-                        priority = Priority.HIGH,
-                        task = "Do coding everyday",
-                        taskDescription = "C++ algorithms and Kotlin"
-                    )
+                    allTasks
                 ) {
 
                 }
